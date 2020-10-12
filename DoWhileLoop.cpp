e90a8aa94507b2f6fd91fd63f6fd877e26337dc6
Pawankumar Chidurala <chpaone09@hotmail.com>
2020-10-13 02:23:25 +0530

    Program to demonstrate DO-WHILE LOOP in cpp

---
+++ DoWhileLoop.cpp
@@ -0,0 +1,22 @@
+/*
+***********************************************************
+
+Topic   : Program to demonstrate DO-WHILE LOOP in cpp.
+Author  : Chpaone09®
+Date    : Oct 12, 2020
+
+************************************************************
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i=1;
+    cout<<"\n\n\t Demonstration of DO-WHILE LOOP in C++ >> ";
+    do{
+        cout<<" "<<i++;
+    }while(i <= 5);
+    return 0;
+}
+
